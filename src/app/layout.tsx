59016559f01db4498dcd0792bb3fe10e7f0a1431
Nick van der Merwe <nickievdm@gmail.com>
2025-10-28 12:21:33 +0200

    Made stuff better v2.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -249,7 +249,7 @@ export default function RootLayout({
               </div>
               <div className="text-center text-[#5c4033] text-sm">
                 <p className="mb-2">• Nick van der Merwe •</p>
-                <p>© 2025 Nicolabs Digital ⚛</p>
+                <p>© 2025 Nicolabs Digital</p>
               </div>
             </div>
           </footer>
